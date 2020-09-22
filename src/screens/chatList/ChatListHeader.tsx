b87fb35cc0339da97b09a91839b7331516226b40
Daehan Chi <chid9202@gmail.com>
2020-09-22 01:37:00 -0500

    add pagetitle on header

--- src/screens/chatList/ChatListHeader.tsx
+++ src/screens/chatList/ChatListHeader.tsx
@@ -1,26 +1,20 @@
 import React from "react";
-import {
-  View,
-  FlatList,
-  StyleSheet,
-  Text,
-  StatusBar,
-  Button,
-  Alert,
-  TouchableOpacity,
-} from "react-native";
+import { View, StyleSheet, Text, Alert, TouchableOpacity } from "react-native";
 import { SystemBlue } from "../../themes";
 import WriteIcon from "../../assets/IconWrite";
 
 const ChatListHeader = () => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => Alert.alert("Edit button pressed!")}>
-        <Text style={styles.editButton}>Edit</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => Alert.alert("Edit button pressed!")}>
-        <WriteIcon />
-      </TouchableOpacity>
+      <View style={styles.interactiveHeader}>
+        <TouchableOpacity onPress={() => Alert.alert("Edit button pressed!")}>
+          <Text style={styles.editButton}>Edit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("Write button pressed!")}>
+          <WriteIcon />
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.pageTitle}>Messages</Text>
     </View>
   );
 };
@@ -28,16 +22,33 @@ const ChatListHeader = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: "column",
+    flexWrap: "nowrap",
     paddingHorizontal: 20,
+    width: "100%",
+    maxHeight: 84,
+  },
+  interactiveHeader: {
+    height: 42,
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    width: "100%",
     justifyContent: "space-between",
     alignItems: "center",
-    width: "100%",
-    maxHeight: 42,
   },
   editButton: {
     color: SystemBlue,
     fontSize: 17,
+  },
+  pageTitle: {
+    width: "100%",
+    height: 42,
+    fontSize: 40,
+    fontWeight: "bold",
+    lineHeight: 41,
+    letterSpacing: 0.374,
+    color: "#ffffff",
   },
 });
 

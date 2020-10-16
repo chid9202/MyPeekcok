e8e8fd541973767e4ccfaf61183fc9dbe41bdd43
Daehan Chi <chid9202@gmail.com>
2020-10-15 23:58:28 -0500

    remove amplify codegen

--- App.tsx
+++ App.tsx
@@ -4,22 +4,40 @@ import ChatDetailScreen from "screens/chatDetail";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { RootStackParamList } from "screens/screenProps";
-import { ApolloProvider } from "@apollo/client";
-import { graphql } from "@apollo/react-hoc";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from "aws-appsync-react";
+// import { graphql } from "@apollo/react-hoc";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.aws_appsync_graphqlEndpoint,
+  region: appSyncConfig.aws_appsync_region,
+  auth: {
+    type: appSyncConfig.aws_appsync_authenticationType,
+    apiKey: appSyncConfig.aws_appsync_apiKey,
+  },
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={ChatListScreen}></Stack.Screen>
-        <Stack.Screen name="Detail" component={ChatDetailScreen}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <Rehydrated>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="Home" component={ChatListScreen}></Stack.Screen>
+            <Stack.Screen
+              name="Detail"
+              component={ChatDetailScreen}
+            ></Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Rehydrated>
+    </ApolloProvider>
   );
 }
